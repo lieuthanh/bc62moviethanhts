b86a9185a7d4bb3a9a5ff0dcb4c850fd550da8de
lieuthanh <155248816+lieuthanh@users.noreply.github.com>
2025-09-11 10:49:48 +0700

    buoi 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import './App.css';
-import HomeTemplate from './pages/HomeTemplate';
-import AdminTemplate from './pages/AdminTemplate';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import renderRoutes from './routes';
 
 function App() {
-  return <div>
-    <HomeTemplate/>
-    <AdminTemplate/>
-  </div>;
+  return <BrowserRouter>
+    <Routes>
+      {renderRoutes()};
+
+      {/* Trang HomeTemplate */}
+      {/* <Route path="" element={<HomeTemplate />}>
+        <Route path="" element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="list-movie" element={<ListMoviePage />} />
+      </Route> */}
+
+      {/* Trang AdminTemplate */}
+      {/* <Route path="admin" element={<AdminTemplate />}>
+        <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="add-user" element={<AddUserPage />} />
+      </Route>
+      <Route path="auth" element={<AuthenPage />} /> */}
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App; 
