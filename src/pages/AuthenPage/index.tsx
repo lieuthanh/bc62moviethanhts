882d163d618db7ac7bf8226d6dd7da10e0364c02
lieuthanh <155248816+lieuthanh@users.noreply.github.com>
2025-09-29 12:00:53 +0700

    buoi 6 tt react hook form validation

--- src/pages/AuthenPage/index.tsx
+++ src/pages/AuthenPage/index.tsx
@@ -3,20 +3,40 @@ import { useDispatch, useSelector } from "react-redux";
 import { actFetchUserLogin } from "./duck/actions";
 import { RootState } from "../../store";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object({
+    taikhoan: yup.string().required("Vui lòng nhập tài khoản"),
+    matkhau: yup.string().required("Vui lòng nhập mật khẩu"),
+});
 
 export default function AuthenPage() {
     const dispatch: any = useDispatch();
     const { loading, data, error } = useSelector((state: RootState) => state.userReducer);
     const navigate = useNavigate();
-    const [values, setValues] = useState({ taikhoan: "", matkhau: "" });
-    const handleOnChange = (event: any) => {
-        const { value, name } = event.target;
-        setValues({ ...values, [name]: value });
-    };
-    const onsubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        dispatch(actFetchUserLogin(values));
+    const { register, handleSubmit, formState } = useForm({
+        defaultValues: {
+            taikhoan: "",
+            matkhau: ""
+        },
+        resolver: yupResolver(schema),
+    });
+
+    // const [values, setValues] = useState({ taikhoan: "", matkhau: "" });
+    // const handleOnChange = (event: any) => {
+    //     const { value, name } = event.target;
+    //     setValues({ ...values, [name]: value });
+    // };
+
+    useEffect(() => {
+        console.log("errors", formState.errors);
+    }, [formState]);
+    const onSubmit = (formValues: any) => {
+        dispatch(actFetchUserLogin(formValues));
     }
+
     useEffect(() => {
         if (data) {
             if (data.maLoaiNguoiDung === "QuanTri") {
@@ -30,9 +50,23 @@ export default function AuthenPage() {
     return (
         <div className="d-flex align-items-center justify-content-center">
             <h3>Đăng nhập</h3>
-            <form onSubmit={onsubmit}>
-                <input type="text" placeholder="Taikhoan" name="taikhoan" onChange={handleOnChange} />
-                <input type="password" placeholder="Matkhau" name="matkhau" onChange={handleOnChange} />
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <input type="text" placeholder="Taikhoan" {...register("taikhoan")} />
+                    {
+                        formState.errors.taikhoan?.message && (
+                            <small className="text-danger">{formState.errors.taikhoan?.message}</small>
+                        )
+                    }
+                </div>
+                <div>
+                    <input type="password" placeholder="Matkhau" {...register("matkhau")} />
+                    {
+                        formState.errors.matkhau?.message && (
+                            <small className="text-danger">{formState.errors.matkhau?.message}</small>
+                        )
+                    }
+                </div>
                 <button disabled={loading} className="btn btn-success" type="submit">{
                     loading && (
                         <div className="spinner-border" role="status">
